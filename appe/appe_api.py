22be23368f0dc0743aaa3d1e06bd70f863c86961
Kamesh <frappe@srv910599.hstgr.cloud>
2025-08-15 08:50:15 +0530

    Chart and Number card added in Mobile app  Dashboard

--- appe/appe_api.py
+++ appe/appe_api.py
@@ -9,6 +9,7 @@ import requests
 from frappe.utils.password import check_password, get_password_reset_limit
 import gzip
 from frappe.utils import get_url
+from frappe.desk.doctype.dashboard_chart.dashboard_chart import get_chart_data
 
 
 @frappe.whitelist()
@@ -489,7 +490,18 @@ def get_dashboard_sections():
         results = []
         for section in app_sections:
             section_items = frappe.get_all('Mobile App Dashboard Items', filters={'parent': section.name}, fields=['*'], order_by="sequence_id")
-            results.append({'section_view': section.get('section_view'),'section_name': section.get('section_name'),'image': section.get('image'),'items': section_items})
+            chartData=[]
+            for item in section_items:
+                if item['linked_doctype'] == "Dashboard Chart":
+                    chart = frappe.get_doc("Dashboard Chart", item['reference_docname'])
+                    chart_data = get_chart_data(chart.name, chart.filters, chart.time_interval)
+
+                    chartData.append({
+                        "chart": chart,
+                        "data": chart_data
+                    })
+
+            results.append({'section_view': section.get('section_view'),'section_name': section.get('section_name'),'image': section.get('image'),'items': section_items, 'chartData': chartData})
         frappe.response.message={'status':True,'message':'','data':results}
         return
     except Exception as e:
