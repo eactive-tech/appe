26cc21435877db94fc4603c423389e732c9f4db2
Kamesh <frappe@srv910599.hstgr.cloud>
2026-02-16 08:49:05 +0530

    Report Print issue resolved

---
+++ appe/appe/doctype/appe_sales_order/appe_sales_order.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Kamesh and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Appe Sales Order", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ appe/appe/doctype/appe_sales_order/test_appe_sales_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Kamesh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAppeSalesOrder(FrappeTestCase):
+	pass
