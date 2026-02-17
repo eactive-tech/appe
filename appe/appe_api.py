3bf0ba2743400c5c9ef68899fe1be7355ca14e0d
Kamesh <frappe@srv910599.hstgr.cloud>
2026-02-17 22:32:13 +0530

    Report Print issue resolved

--- appe/appe_api.py
+++ appe/appe_api.py
@@ -369,7 +369,7 @@ def gettasks_and_request_and_attendancedata():
         pending_approvals = frappe.get_all(
             "Workflow Action",
             fields=["*"],
-            filters={"status": "Open", "user":user},
+            filters=[["Workflow Action","user","=",user],["Workflow Action","status","=","Open"]]
             or_filters=[["Workflow Action Permitted Role","role", "in", roles]],
             distinct=True
         )
